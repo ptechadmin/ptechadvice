ea2ce0f0675abaf7963867ff2d117cafba91ae1d
ptechadmin <technical.pcguide@gmail.com>
2022-01-23 10:15:10 -0500

    website update
    add changes to meta and favicons

---
+++ scripts/sent.js
@@ -0,0 +1,6 @@
+//function submitPoll(id){
+//      document.getElementById("sent").disabled = true;
+//      setTimeout(function(){document.getElementById("sent").disabled = false;},30000);
+//  }
+
+
