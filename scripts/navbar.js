a738e21130f46f7c67632c8f1d6a36f8d6bc6853
ptechadmin <technical.pcguide@gmail.com>
2022-01-28 23:25:23 -0500

    update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,24 @@
 var prevScrollpos = window.pageYOffset;
 	window.onscroll = function() {
 		var currentScrollPos = window.pageYOffset;
-			if (prevScrollpos > currentScrollPos) {
+			
+			  
+	if(navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
+		if (prevScrollpos > currentScrollPos) {
+				document.getElementById("nav").style.top = "0";
+			  } else {
+				document.getElementById("nav").style.top = "0";
+			  }
+			  prevScrollpos = currentScrollPos;
+	}else {
+		if (prevScrollpos > currentScrollPos) {
 				document.getElementById("nav").style.top = "0";
 			  } else {
 				document.getElementById("nav").style.top = "-80px";
 			  }
 			  prevScrollpos = currentScrollPos;
+		
+		
+		
+	}
 } 
